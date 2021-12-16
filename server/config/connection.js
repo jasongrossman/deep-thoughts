1977b67d2d7a053c2d5e93fc167af71ba645be2f
jasongrossman <85508030+jasongrossman@users.noreply.github.com>
2021-12-16 11:57:35 -0500

    mongodb setup

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/deep-thoughts',
+  process.env.MONGODB_URI || 'mongodb+srv://jasongrossman:VVu3Hq/hy^V2rhP@cluster0.z3nv6.mongodb.net/deep-thoughts?retryWrites=true&w=majority',
   {
     useNewUrlParser: true, 
     useUnifiedTopology: true,
